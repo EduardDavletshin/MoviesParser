f0aa1a61fbaef07b3efdc457dc1ee014065b7c25
EduardDavletshin <eduard.davletshin@flatstack.com>
2017-01-09 15:19:02 +0300

    Retrofit request casted into AsyncTask

--- app/src/main/java/com/example/eddy/moviesparser/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/eddy/moviesparser/RecyclerViewAdapter.java
@@ -1,6 +1,5 @@
 package com.example.eddy.moviesparser;
 
-import android.content.Context;
 import android.content.res.Resources;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
